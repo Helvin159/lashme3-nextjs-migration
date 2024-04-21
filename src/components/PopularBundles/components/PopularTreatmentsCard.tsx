6eb6fbfe760cbf3996872cd3e2e2047ea89b04a3
helvin159 <helvin159@gmail.com>
2024-04-21 19:04:28 -0400

    new updates

--- src/components/PopularBundles/components/PopularTreatmentsCard.tsx
+++ src/components/PopularBundles/components/PopularTreatmentsCard.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 const PopularTreatmentsCard = ({ service }: any) => {
 	return (
-		<div className='flex mb-2 px-4 tablet:mb-0 tablet:px-0'>
-			<div className='p-0 h-full w-full tablet:pr-1'>
+		<div className='flex mb-2 px-4 tablet:mb-0 tablet:px-0 self-start'>
+			<div className='flex p-0 h-full w-3/6 tablet:pr-1 self-start'>
 				<Link href={`/services/${service.fields.slug}`}>
 					<Image
 						src={`https:${service.fields.samplePictures[0].fields.file.url}`}
@@ -20,8 +20,8 @@ const PopularTreatmentsCard = ({ service }: any) => {
 					/>
 				</Link>
 			</div>
-			<div className='flex flex-col max-h-19rem p-0 justify-between pl-1 tablet:pr-2'>
-				<div className='h-3/6 w-full self-start pb-1'>
+			<div className='flex flex-col w-3/6 max-h-19rem p-0 justify-between pl-1 tablet:pr-2'>
+				<div className=' flex h-3/6 w-full  self-start pb-1'>
 					<Link href={`/services/${service.fields.slug}`}>
 						<Image
 							src={`https:${service.fields.samplePictures[1]?.fields.file.url}`}
@@ -32,12 +32,12 @@ const PopularTreatmentsCard = ({ service }: any) => {
 								service.fields.samplePictures[1].fields.file.details.image
 									.height
 							}
-							className='object-cover object-center w-full h-full rounded-xl'
+							className='object-cover object-center h-full rounded-xl'
 							alt=''
 						/>
 					</Link>
 				</div>
-				<div className='h-3/6 w-full self-end pt-1'>
+				<div className=' flex h-3/6 w-full  self-end pt-1'>
 					<Link href={`/services/${service.fields.slug}`}>
 						<Image
 							src={`https:${service.fields.samplePictures[2]?.fields.file.url}`}
@@ -48,7 +48,7 @@ const PopularTreatmentsCard = ({ service }: any) => {
 								service.fields.samplePictures[2].fields.file.details.image
 									.height
 							}
-							className='object-cover object-center w-full h-full rounded-xl'
+							className='object-cover object-center h-full rounded-xl'
 							alt=''
 						/>
 					</Link>
